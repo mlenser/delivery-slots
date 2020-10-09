beab22435b0ddfff26df12703dc3bb37eda87917
Mark Lenser <mlenser@gmail.com>
2020-10-09 18:54:34 +0200

    refactor: disabled button based on selected date and time

--- src/containers/date-selector/SubmitButton.tsx
+++ src/containers/date-selector/SubmitButton.tsx
@@ -3,15 +3,15 @@ import Button from '@material-ui/core/Button';
 import { useDeliveryContext } from './DeliveryContext';
 
 const SubmitButton: React.FC = () => {
-  const { homeDelivery } = useDeliveryContext();
+  const { selectedDate, selectedTime } = useDeliveryContext();
 
   // UX comments: It is not a good practice to disable a button like this as the user then has no means of knowing what steps must be completed before they can proceed. It would be much better to always have it enabled and show an error message if the required fields are not set. But for this code exercise I have followed the specifications
   return (
     <section>
       <Button
-        className={homeDelivery ? '' : 'disabled'}
+        className={selectedDate && selectedTime ? '' : 'disabled'}
         color="primary"
-        disabled={!homeDelivery}
+        disabled={!(selectedDate && selectedTime)}
         type="submit"
         variant="contained"
       >
