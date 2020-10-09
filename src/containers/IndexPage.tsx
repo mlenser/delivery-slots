9c7dbbf2d34c02c921b3421e9281640cb21edeb1
Mark Lenser <mlenser@gmail.com>
2020-10-09 14:10:19 +0200

    feat: show all the possible delivery dates

--- src/containers/IndexPage.tsx
+++ src/containers/IndexPage.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import H1 from '../components/Headings/H1';
+import H2 from '../components/Headings/H2';
 
 const Article = styled.article`
   text-align: center;
@@ -10,11 +11,28 @@ type Props = {
   title: string;
 };
 
-const IndexPage: React.FC<Props> = ({ title }) => (
-  <Article>
-    <H1>{title}</H1>
+const IndexPage: React.FC<Props> = ({ title }) => {
+  const [datesData, setDatesData] = useState([]);
 
-  </Article>
-);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        'https://api.mathem.io/mh-test-assignment/delivery/dates',
+      );
+      const data = await response.json();
+      setDatesData(data);
+    })();
+  }, []);
+
+  return (
+    <Article>
+      <H1>{title}</H1>
+      <H2>Dates</H2>
+      {datesData?.map((date) => (
+        <div key={date}>{date}</div>
+      ))}
+    </Article>
+  );
+};
 
 export default IndexPage;
