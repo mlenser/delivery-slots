120b8d56af5cca080da20123ea9fd638ad2bf479
Mark Lenser <mlenser@gmail.com>
2020-10-09 17:27:19 +0200

    feat: add link to the confirmation page to go back

--- src/containers/ConfirmationPage.tsx
+++ src/containers/ConfirmationPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import H1 from '../components/Headings/H1';
+import H2 from '../components/Headings/H2';
+import Link from '../components/Link';
 
 const Article = styled.article`
   text-align: center;
@@ -9,6 +11,11 @@ const Article = styled.article`
 const ConfirmationPage: React.FC = () => (
   <Article>
     <H1>Confirmation</H1>
+    <p>
+      Made a mistake? <Link to="/">Edit the delivery date and time</Link>
+    </p>
+
+    <H2>Delivery details</H2>
   </Article>
 );
 
