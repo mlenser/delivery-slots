e61a910b4934a9d16e2f7cb3bbdeaf5a4cd64b4a
Mark Lenser <mlenser@gmail.com>
2020-10-10 11:43:02 +0200

    refactor: remove unused

--- src/components/Headings/H5.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import HeadingBase from './HeadingBase';
-
-const H5 = styled.h5<{ firstItem?: boolean }>`
-  ${HeadingBase};
-  font-size: ${({ theme }) => theme.sizes.normal};
-`;
-
-export default H5;
--- src/components/Headings/H6.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import HeadingBase from './HeadingBase';
-
-const H6 = styled.h6<{ firstItem?: boolean }>`
-  ${HeadingBase};
-  font-size: ${({ theme }) => theme.sizes.small};
-`;
-
-export default H6;
